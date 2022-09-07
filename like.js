b3e661fd7357a43bd53740c4c653c7ee586b2055
Ethanhe213 <112043672+Ethanhe213@users.noreply.github.com>
2022-09-06 23:25:42 -0400

    Add files via upload

---
+++ like.js
@@ -0,0 +1,9 @@
+var x=2
+var y=document.querySelector("#add1")
+
+
+function add(){
+    x++;
+    y.innerText=x+" "+"Likes"
+
+}
---
+++ like2.js
@@ -0,0 +1,20 @@
+var a=3;
+var b=0;
+var c=0;
+var x= document.querySelector("#like1");
+var y= document.querySelector("#like2");
+var z= document.querySelector("#like3");
+
+function add1(){
+    a++;
+    x.innerText=a+" "+"like(s)";
+}
+function add2(){
+    b++;
+    y.innerText=b+" "+"like(s)";
+}
+function add3(){
+    c++;
+    z.innerText=c+" "+"like(s)";
+}
+
